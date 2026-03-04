733d1aecf5adcc895b95925c90d5955b32d6232d
Felipe Iraola <felipe.iraola@gmail.com>
2026-03-03 21:17:57 -0300

    Pagina completa

---
+++ scripts/codigo.js
@@ -0,0 +1,10 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(a => {
+    a.addEventListener('click', e => {
+        e.preventDefault();
+        const el = document.querySelector(a.getAttribute('href'));
+        if (el) el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    });
+});
